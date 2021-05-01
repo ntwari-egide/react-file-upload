d11f8e7cfc8e3af9601550a975c22a95ddc37251
yombi <ntwariegide2@gmail.com>
2021-05-01 20:18:36 +0200

    working with file upload

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="container" style={{ width: "600px" }}>
       <div style={{ margin: "20px" }}>
-        <h3>bezkoder.com</h3>
+        <h3>Yombi.com file upload tester</h3>
         <h4>React upload Files</h4>
       </div>
 
--- src/services/upload-files.service.js
+++ src/services/upload-files.service.js
@@ -9,6 +9,7 @@ class UploadFilesService {
     return http.post("/uploadProfilePic/6", formData, {
       headers: {
         "Content-Type": "multipart/form-data",
+        "Authorization": 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1d2lzaGVtYUBnbWFpbC5jb20iLCJleHAiOjE2MTk5MzYwMjQsImlhdCI6MTYxOTg5MjgyNH0.jqPJhoZDdBWc_8Ldknjxqs0k2jnnWDRZu5G2zxPeWeD1V8X0vOGHppY3jyoZ_Av7owqsJ_OflVVJBi06qzgNfA'
       },
       onUploadProgress,
     });
