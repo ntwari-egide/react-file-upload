c6e505105c380bf2e6732aec36d273fa3560a00b
yombi <ntwariegide2@gmail.com>
2021-05-01 20:52:45 +0200

    working with file upload

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://197.243.22.47:8088/yombi-app-backend",
+  baseURL: "localhost:8080",
   headers: {
     "Content-type": "application/json"
   }
